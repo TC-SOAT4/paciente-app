282a78d040ffdf417e50139e1bce9b8d59a2c61b
Thiago <thiagobsn@gmail.com>
2024-07-26 15:30:27 -0300

    Conf k8s

--- src/main/resources/db/migration/V02__insert-table-paciente.sql
+++ src/main/resources/db/migration/V02__insert-table-paciente.sql
@@ -1,5 +1,5 @@
 INSERT INTO Paciente (id, nome, cpf, email) VALUES ('75a1fe66-c4da-432b-80ef-509b2eef4ae7', 'Sansa Stark', '12312312326', 'sansa.stark@email.com.br');
-INSERT INTO Paciente (id, nome, cpf, email) VALUES ('8675bc70-cae5-49a0-bf31-65e5cdae3b15', 'Jon Snow', '12312312327', 'snow.jon@email.com.br');
+INSERT INTO Paciente (id, nome, cpf, email) VALUES ('8675bc70-cae5-49a0-bf31-65e5cdae3b15', 'Jon Snow', '12345678912', 'snow.jon@email.com.br');
 INSERT INTO Paciente (id, nome, cpf, email) VALUES ('59ba341f-50d0-4635-b11b-72bd9363019d', 'Daenerys Targaryen', '12312312328', 'daenerys@email.com.br');
 INSERT INTO Paciente (id, nome, cpf, email) VALUES ('1cdcf7a5-11b4-4d9e-b1f0-00f5fe3435b6', 'Tyrion Lannister', '12312312329', 'tyrion.lannister@email.com.br');
 INSERT INTO Paciente (id, nome, cpf, email) VALUES ('ff309ee5-b9e1-449c-b6d6-ccdbbbb53e27', 'Arya Stark', '12312312330', 'arya@email.com.br');
